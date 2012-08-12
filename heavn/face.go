c85d2ba71dd675411ffd466fe2b78ad0a678cd96
berak <px1704@web.de>
2012-08-12 18:14:37 +0200

    0002

--- heavn/face.go
+++ heavn/face.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"math"
+    "math"
 )
 
 var unindices []int = nil
@@ -11,13 +11,13 @@ type Histogram []uint16
 
 
 type PicRec struct {
-	Name string
-	Hist []byte
+    Name string
+    Hist []byte
 }
 
 type Matrix struct {
-	e    []uint8
-	w, h int
+    e    []uint8
+    w, h int
 }
 
 type Sampler func(m *Matrix, pos,radius int) uint8
@@ -25,125 +25,125 @@ type Sampler func(m *Matrix, pos,radius int) uint8
 
 
 func createMatrix(w, h int) *Matrix {
-	var m Matrix
-	m.w = w
-	m.h = h
-	m.e = make([]uint8, w*h)
-	return &m
+    var m Matrix
+    m.w = w
+    m.h = h
+    m.e = make([]uint8, w*h)
+    return &m
 }
 func (m *Matrix) pos(x, y int) int {
-	return (m.w*y + x)
+    return (m.w*y + x)
 }
 func (m *Matrix) set(x, y int, val uint8) {
-	m.e[m.w*y+x] = val
+    m.e[m.w*y+x] = val
 }
 func (m *Matrix) get(x, y int) uint8 {
-	return m.e[m.w*y+x]
+    return m.e[m.w*y+x]
 }
 
 func getbit(m *Matrix, pos int, cen uint8) uint8 {
-	if m.e[pos] >= cen {
-		return 1
-	}
-	return 0
+    if m.e[pos] >= cen {
+        return 1
+    }
+    return 0
 }
 
 // Sampler
 func square(m *Matrix, pos,radius int) (way uint8) {
-	cen := m.e[pos]
-	way = 0
-	way |= getbit(m, pos-radius*m.w,        cen) << 0
-	way |= getbit(m, pos-radius*m.w+radius, cen) << 1
-	way |= getbit(m, pos+radius,            cen) << 2
-	way |= getbit(m, pos+radius*m.w+radius, cen) << 3
-	way |= getbit(m, pos+radius*m.w,        cen) << 4
-	way |= getbit(m, pos+radius*m.w-radius, cen) << 5
-	way |= getbit(m, pos-radius,            cen) << 6
-	way |= getbit(m, pos-radius*m.w-radius, cen) << 7
-	return
+    cen := m.e[pos]
+    way = 0
+    way |= getbit(m, pos-radius*m.w,        cen) << 0
+    way |= getbit(m, pos-radius*m.w+radius, cen) << 1
+    way |= getbit(m, pos+radius,            cen) << 2
+    way |= getbit(m, pos+radius*m.w+radius, cen) << 3
+    way |= getbit(m, pos+radius*m.w,        cen) << 4
+    way |= getbit(m, pos+radius*m.w-radius, cen) << 5
+    way |= getbit(m, pos-radius,            cen) << 6
+    way |= getbit(m, pos-radius*m.w-radius, cen) << 7
+    return
 }
 
 // Sampler
 func circle(m *Matrix, pos,radius int) (way uint8) {
-	cen := m.e[pos]
-	tmpway := uint8(0)
-	way = 0
-	way    |= getbit(m, pos-m.w, cen) << 0
-	tmpway  = getbit(m, pos-m.w+1, cen)
-	tmpway += getbit(m, pos+1, cen)
-	tmpway += getbit(m, pos, cen)
-	tmpway += getbit(m, pos-m.w, cen)
-	if tmpway >= 2 {
-		way |= 1 << 1
-	}
-	way    |= getbit(m, pos+1, cen) << 2
-	tmpway  = getbit(m, pos+m.w+1, cen)
-	tmpway += getbit(m, pos+m.w, cen)
-	tmpway += getbit(m, pos, cen)
-	tmpway += getbit(m, pos+1, cen)
-	if tmpway >= 2 {
-		way |= 1 << 3
-	}
-	way    |= getbit(m, pos+m.w, cen) << 4
-	tmpway  = getbit(m, pos+m.w-1, cen)
-	tmpway += getbit(m, pos-1, cen)
-	tmpway += getbit(m, pos, cen)
-	tmpway += getbit(m, pos+m.w, cen)
-	if tmpway >= 2 {
-		way |= 1 << 5
-	}
-	way    |= getbit(m, pos-1, cen) << 6
-	tmpway  = getbit(m, pos-m.w-1, cen)
-	tmpway += getbit(m, pos-m.w, cen)
-	tmpway += getbit(m, pos, cen)
-	tmpway += getbit(m, pos-1, cen)
-	if tmpway >= 2 {
-		way |= 1 << 7
-	}
-	return
+    cen := m.e[pos]
+    tmpway := uint8(0)
+    way = 0
+    way    |= getbit(m, pos-m.w, cen) << 0
+    tmpway  = getbit(m, pos-m.w+1, cen)
+    tmpway += getbit(m, pos+1, cen)
+    tmpway += getbit(m, pos, cen)
+    tmpway += getbit(m, pos-m.w, cen)
+    if tmpway >= 2 {
+        way |= 1 << 1
+    }
+    way    |= getbit(m, pos+1, cen) << 2
+    tmpway  = getbit(m, pos+m.w+1, cen)
+    tmpway += getbit(m, pos+m.w, cen)
+    tmpway += getbit(m, pos, cen)
+    tmpway += getbit(m, pos+1, cen)
+    if tmpway >= 2 {
+        way |= 1 << 3
+    }
+    way    |= getbit(m, pos+m.w, cen) << 4
+    tmpway  = getbit(m, pos+m.w-1, cen)
+    tmpway += getbit(m, pos-1, cen)
+    tmpway += getbit(m, pos, cen)
+    tmpway += getbit(m, pos+m.w, cen)
+    if tmpway >= 2 {
+        way |= 1 << 5
+    }
+    way    |= getbit(m, pos-1, cen) << 6
+    tmpway  = getbit(m, pos-m.w-1, cen)
+    tmpway += getbit(m, pos-m.w, cen)
+    tmpway += getbit(m, pos, cen)
+    tmpway += getbit(m, pos-1, cen)
+    if tmpway >= 2 {
+        way |= 1 << 7
+    }
+    return
 }
 
 func rectbit(m *Matrix, pos int, cen uint8) (uint8) {
-	way := getbit(m, pos, cen)
-	way += getbit(m, pos+1, cen)
-	way += getbit(m, pos+m.w, cen)
-	way += getbit(m, pos+m.w+1, cen)
-	if way >= 2 { return 1 }
-	return 0
+    way := getbit(m, pos, cen)
+    way += getbit(m, pos+1, cen)
+    way += getbit(m, pos+m.w, cen)
+    way += getbit(m, pos+m.w+1, cen)
+    if way >= 2 { return 1 }
+    return 0
 }
 // Sampler
 func square2(m *Matrix, pos,radius int) (way uint8) {
-	cen := m.e[pos]
-	way = 0
-	way |= rectbit(m, pos-radius*m.w,            cen) << 0
-	way |= rectbit(m, pos-radius*m.w+radius-1,   cen) << 1
-	way |= rectbit(m, pos+radius-1,              cen) << 2
-	way |= rectbit(m, pos+(radius-1)*m.w+radius-1, cen) << 3
-	way |= rectbit(m, pos+(radius-1)*m.w,        cen) << 4
-	way |= rectbit(m, pos+(radius-1)*m.w-radius, cen) << 5
-	way |= rectbit(m, pos-radius,                cen) << 6
-	way |= rectbit(m, pos-radius*m.w-radius,     cen) << 7
-	return
+    cen := m.e[pos]
+    way = 0
+    way |= rectbit(m, pos-radius*m.w,            cen) << 0
+    way |= rectbit(m, pos-radius*m.w+radius-1,   cen) << 1
+    way |= rectbit(m, pos+radius-1,              cen) << 2
+    way |= rectbit(m, pos+(radius-1)*m.w+radius-1, cen) << 3
+    way |= rectbit(m, pos+(radius-1)*m.w,        cen) << 4
+    way |= rectbit(m, pos+(radius-1)*m.w-radius, cen) << 5
+    way |= rectbit(m, pos-radius,                cen) << 6
+    way |= rectbit(m, pos-radius*m.w-radius,     cen) << 7
+    return
 }
 // Sampler
 func circle2(m *Matrix, pos,radius int) (way uint8) {
-	cen := m.e[pos]
-	way = 0
-	way |= getbit( m, pos-radius*m.w, 			    cen) << 0
-	way |= rectbit(m, pos-radius*m.w+(radius-1),    cen) << 1
-	way |= getbit( m, pos+radius, 			        cen) << 2
-	way |= rectbit(m, pos+(radius-1)+(radius-1)*m.w,cen) << 3
-	way |= getbit( m, pos+radius*m.w, 	            cen) << 4
-	way |= rectbit(m, pos-(radius)-(radius-1)*m.w,  cen) << 5
-	way |= getbit( m, pos-radius,			        cen) << 6
-	way |= rectbit(m, pos-radius-radius*m.w,        cen) << 7
-	return
+    cen := m.e[pos]
+    way = 0
+    way |= getbit( m, pos-radius*m.w,                 cen) << 0
+    way |= rectbit(m, pos-radius*m.w+(radius-1),    cen) << 1
+    way |= getbit( m, pos+radius,                     cen) << 2
+    way |= rectbit(m, pos+(radius-1)+(radius-1)*m.w,cen) << 3
+    way |= getbit( m, pos+radius*m.w,                 cen) << 4
+    way |= rectbit(m, pos-(radius)-(radius-1)*m.w,  cen) << 5
+    way |= getbit( m, pos-radius,                    cen) << 6
+    way |= rectbit(m, pos-radius-radius*m.w,        cen) << 7
+    return
 }
 
 // Sampler
 func elbp(m *Matrix, pos,radius int) (way uint8) {
-	way = 0
-	cen := m.e[pos]
+    way = 0
+    cen := m.e[pos]
     for n:=uint8(0); n<8; n++ {
         x  := float64(-radius) * math.Sin(math.Pi*float64(n)/4.0);
         y  := float64( radius) * math.Cos(math.Pi*float64(n)/4.0);
@@ -159,54 +159,54 @@ func elbp(m *Matrix, pos,radius int) (way uint8) {
         w2 :=        tx  * (1.0 - ty);
         w3 := (1.0 - tx) *        ty;
         w4 :=        tx  *        ty;
-		t  := w1*float64(m.e[pos + int(fx) + int(fy)*m.w]);
-		t  += w2*float64(m.e[pos + int(cx) + int(fy)*m.w]);
-		t  += w3*float64(m.e[pos + int(fx) + int(cy)*m.w]);
-		t  += w4*float64(m.e[pos + int(cx) + int(cy)*m.w]);
-		if uint8(t) > cen {
-			way |= 1<<n
-		}
+        t  := w1*float64(m.e[pos + int(fx) + int(fy)*m.w]);
+        t  += w2*float64(m.e[pos + int(cx) + int(fy)*m.w]);
+        t  += w3*float64(m.e[pos + int(fx) + int(cy)*m.w]);
+        t  += w4*float64(m.e[pos + int(cx) + int(cy)*m.w]);
+        if uint8(t) > cen {
+            way |= 1<<n
+        }
     }
-	return
+    return
 }
 
 //
 // unified spatial(7x7) histogram with preprocessed indices
 //
 func (m *Matrix) histogram(sample Sampler, radius int) *Histogram {
-	ncell := 7
-	nhist := 57
-	cw  := int(m.w / (ncell-1))
-	ch  := int(m.h / (ncell-1))
-	hist := make(Histogram, nhist*(ncell*ncell))
-	for y := radius; y < m.h-radius; y++ {
-		for x := radius; x < m.w-radius; x++ {
-			way := sample(m, m.pos(x, y),radius)
-			idx := unindices[way]
+    ncell := 7
+    nhist := 57
+    cw  := int(m.w / (ncell-1))
+    ch  := int(m.h / (ncell-1))
+    hist := make(Histogram, nhist*(ncell*ncell))
+    for y := radius; y < m.h-radius; y++ {
+        for x := radius; x < m.w-radius; x++ {
+            way := sample(m, m.pos(x, y),radius)
+            idx := unindices[way]
 
-			cx  := int(x / cw)
-			cy  := int(y / ch)
-			ch  := (cx + cy * ncell) * nhist
+            cx  := int(x / cw)
+            cy  := int(y / ch)
+            ch  := (cx + cy * ncell) * nhist
 
-			if idx != -1 {
-				hist[ch + idx] += 1
-			} else {
-				// assign non-uniform ones to the 'noise' channel
-				hist[ch + nhist-1] += 1
-			}
-		}
-	}
-	return &hist
+            if idx != -1 {
+                hist[ch + idx] += 1
+            } else {
+                // assign non-uniform ones to the 'noise' channel
+                hist[ch + nhist-1] += 1
+            }
+        }
+    }
+    return &hist
 }
 
 func norml2(a, b *Histogram) float64 {
-	if len(*a) != len(*b) {return -1}
-	dist := int(0)
-	for ai, av := range *a {
-		d := int(av) - int((*b)[ai])
-		dist += d * d
-	}
-	return math.Sqrt(float64(dist))
+    if len(*a) != len(*b) {return -1}
+    dist := int(0)
+    for ai, av := range *a {
+        d := int(av) - int((*b)[ai])
+        dist += d * d
+    }
+    return math.Sqrt(float64(dist))
 }
 
 
@@ -216,32 +216,32 @@ func norml2(a, b *Histogram) float64 {
 // in the uniform u8 case, there are only 57 of 255 valid cases
 //
 func bit(b, i uint8) bool {
-	return ((b & (1 << i)) != 0)
+    return ((b & (1 << i)) != 0)
 }
 func uniform(way uint8) bool {
-	cu := 0
-	if bit(way, 0) != bit(way, 1) {	cu += 1	}
-	if bit(way, 1) != bit(way, 2) {	cu += 1	}
-	if bit(way, 2) != bit(way, 3) {	cu += 1	}
-	if bit(way, 3) != bit(way, 4) {	cu += 1	}
-	if bit(way, 4) != bit(way, 5) {	cu += 1	}
-	if bit(way, 5) != bit(way, 6) {	cu += 1	}
-	if bit(way, 6) != bit(way, 7) {	cu += 1	}
-	if bit(way, 7) != bit(way, 0) {	cu += 1	}
-	return (cu <= 2)
+    cu := 0
+    if bit(way, 0) != bit(way, 1) {    cu += 1    }
+    if bit(way, 1) != bit(way, 2) {    cu += 1    }
+    if bit(way, 2) != bit(way, 3) {    cu += 1    }
+    if bit(way, 3) != bit(way, 4) {    cu += 1    }
+    if bit(way, 4) != bit(way, 5) {    cu += 1    }
+    if bit(way, 5) != bit(way, 6) {    cu += 1    }
+    if bit(way, 6) != bit(way, 7) {    cu += 1    }
+    if bit(way, 7) != bit(way, 0) {    cu += 1    }
+    return (cu <= 2)
 }
 func initIndices() {
-	iu := 0
-	unindices = make([]int, 257)
-	unindices[256] = -1
-	for i := uint8(0); i < 0xff; i++ {
-		if uniform(i) {
-			unindices[i] = iu
-			iu += 1
-		} else {
-			unindices[i] = -1
-		}
-	}
+    iu := 0
+    unindices = make([]int, 257)
+    unindices[256] = -1
+    for i := uint8(0); i < 0xff; i++ {
+        if uniform(i) {
+            unindices[i] = iu
+            iu += 1
+        } else {
+            unindices[i] = -1
+        }
+    }
 }
 
 
--- heavn/http.go
+++ heavn/http.go
@@ -1,78 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"net/http"
+    "fmt"
+    "math"
+    "net/http"
     "sort"
     //~ "strconv"
-	
+    
     "appengine"
     "appengine/datastore"
-	"appengine/memcache"
-	
-	"io"
-	"bytes"
-	"encoding/binary"
-	//~ "compress/zlib"
+    "appengine/memcache"
+    
+    "io"
+    "bytes"
+    "encoding/binary"
+    //~ "compress/zlib"
 
-	"image"
-	"image/jpeg"
-	_ "image/color"
-	_ "image/gif"
-	_ "image/png"
+    "image"
+    "image/jpeg"
+    _ "image/color"
+    _ "image/gif"
+    _ "image/png"
 )
 
 
 type Thumbnail struct {
-	Name string
-	Pic []byte
+    Name string
+    Pic []byte
 }
 
 
 
 
 func handle(m image.Image, r image.Rectangle) (mat *Matrix) {
-	i := 0
-	mat = createMatrix(r.Max.X-r.Min.X,r.Max.Y-r.Max.Y)
+    i := 0
+    mat = createMatrix(r.Max.X-r.Min.X,r.Max.Y-r.Max.Y)
     for y := r.Min.Y; y < r.Max.Y; y++ {
         for x := r.Min.X; x < r.Max.X; x++ {
-			r8,g8,b8,_ := m.At(x,y).RGBA()
-			mat.e[i] = uint8((r8>>2) + (g8>>1) + (b8>>2))
-			i += 1
-		}
-	}
-	return
+            r8,g8,b8,_ := m.At(x,y).RGBA()
+            mat.e[i] = uint8((r8>>2) + (g8>>1) + (b8>>2))
+            i += 1
+        }
+    }
+    return
 }
 
 func handleRGBA(m *image.RGBA) (mat *Matrix) {
-	i := 0
+    i := 0
     r := m.Bounds()
-	mat = createMatrix(r.Max.X,r.Max.Y)
+    mat = createMatrix(r.Max.X,r.Max.Y)
     for y := r.Min.Y; y < r.Max.Y; y++ {
         pix := m.Pix[y*m.Stride:]
         for x := r.Min.X; x < r.Max.X; x++ {
             r8 := uint8(pix[x*4])
             g8 := uint8(pix[x*4+1])
             b8 := uint8(pix[x*4+2])
-			mat.e[i] = uint8((r8>>2) + (g8>>1) + (b8>>2))
-			i += 1
-		}
-	}
-	return
+            mat.e[i] = uint8((r8>>2) + (g8>>1) + (b8>>2))
+            i += 1
+        }
+    }
+    return
 }
 func handleYCbCr(m *image.YCbCr)  (mat *Matrix) {
-	i := 0
+    i := 0
     r := m.Bounds()
-	mat = createMatrix(r.Max.X,r.Max.Y)
+    mat = createMatrix(r.Max.X,r.Max.Y)
     for y := r.Min.Y; y < r.Max.Y; y++ {
         for x := r.Min.X; x < r.Max.X; x++ {
-			r8,g8,b8,_ := m.At(x,y).RGBA()
-			mat.e[i] = uint8((r8>>2) + (g8>>1) + (b8>>2))
-			i += 1
-		}
-	}
-	return;
+            r8,g8,b8,_ := m.At(x,y).RGBA()
+            mat.e[i] = uint8((r8>>2) + (g8>>1) + (b8>>2))
+            i += 1
+        }
+    }
+    return;
 }
 
 
@@ -85,38 +85,38 @@ func fillDict(c appengine.Context) {
         c.Warningf("%v",err)
         return
     }
-	persons = make(map[string]*Histogram)
+    persons = make(map[string]*Histogram)
     for _, p := range(pr) {
-		hist := make(Histogram,len(p.Hist)/2)
-		buf  := bytes.NewBuffer(p.Hist)
-		binary.Read(buf, binary.LittleEndian, hist)
-		persons[p.Name] = &hist
+        hist := make(Histogram,len(p.Hist)/2)
+        buf  := bytes.NewBuffer(p.Hist)
+        binary.Read(buf, binary.LittleEndian, hist)
+        persons[p.Name] = &hist
     }
-	c.Infof("loaded %v persons",len(persons) )
+    c.Infof("loaded %v persons",len(persons) )
 }
 
 type KSarr []string 
-func (s KSarr) Len() int 		   { return len(s) }
+func (s KSarr) Len() int            { return len(s) }
 func (s KSarr) Less(i, j int) bool { return s[i] < s[j] }
 func (s KSarr) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 func listDict() string {
-	var s string = "<table>"
-	i := 0
-	lp := len(persons)
-	arr := make(KSarr,lp)	
+    var s string = "<table>"
+    i := 0
+    lp := len(persons)
+    arr := make(KSarr,lp)    
     for a, _ := range(persons) {
-		arr[i]=a;
-		if i+=1; i>=1000 { break }
-	}
-	sort.Sort(arr)
-    for i=0; i<len(arr); i++ {
-		if i % 12 == 0 {
-			s += "</tr>\n<tr>"
-		}
-		s += fmt.Sprintf("<td><a href=/?match=%v>%v</a>&nbsp;&nbsp; </td>",arr[i],arr[i])
+        arr[i]=a;
+        if i+=1; i>=1000 { break }
     }
-	return s + "\n</tr></table>\n"
+    sort.Sort(arr)
+    for i=0; i<len(arr); i++ {
+        if i % 12 == 0 {
+            s += "</tr>\n<tr>"
+        }
+        s += fmt.Sprintf("<td><a href=/?match=%v>%v</a>&nbsp;&nbsp; </td>",arr[i],arr[i])
+    }
+    return s + "\n</tr></table>\n"
 }
 
 
@@ -126,184 +126,184 @@ var persons map[string]*Histogram
 
 
 func init() {
-	initIndices()
-	
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/thumb", handler_thumb)
-	http.HandleFunc("/reset", handler_reset)
+    initIndices()
+    
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/thumb", handler_thumb)
+    http.HandleFunc("/reset", handler_reset)
 }
 
 
 func handler_thumb(w http.ResponseWriter, r *http.Request) {
     r.ParseForm()
     c := appengine.NewContext(r)
-	name := r.FormValue("id")
+    name := r.FormValue("id")
 
-	var thumb Thumbnail
-	if item, err := memcache.Get(c, name); err == nil {
-		thumb = Thumbnail{Name:item.Key, Pic:item.Value}
-	} else {
-		q := datastore.NewQuery("Thumbnail").Filter("Name =", name )
-		for t := q.Run(c); ; {
-			_, err := t.Next(&thumb)
-			if err != nil {
-				fmt.Fprint(w, "<html><head> ", style, "<title>;(</title></head><body>")
-				fmt.Fprint(w, "<h4>can't find thumb for ", name)
-				return
-			}
+    var thumb Thumbnail
+    if item, err := memcache.Get(c, name); err == nil {
+        thumb = Thumbnail{Name:item.Key, Pic:item.Value}
+    } else {
+        q := datastore.NewQuery("Thumbnail").Filter("Name =", name )
+        for t := q.Run(c); ; {
+            _, err := t.Next(&thumb)
+            if err != nil {
+                fmt.Fprint(w, "<html><head> ", style, "<title>;(</title></head><body>")
+                fmt.Fprint(w, "<h4>can't find thumb for ", name)
+                return
+            }
 
-			// Add the item to the memcache, if the key does not already exist
-			item := &memcache.Item{
-				Key:   name,
-				Value: thumb.Pic,
-			}
-			if err := memcache.Add(c, item); err == memcache.ErrNotStored {
-				c.Warningf("item with key %q already exists", item.Key)
-			} else if err != nil {
-				c.Warningf("error adding item: %v", err)
-			}
-			break
-		}
-	}
+            // Add the item to the memcache, if the key does not already exist
+            item := &memcache.Item{
+                Key:   name,
+                Value: thumb.Pic,
+            }
+            if err := memcache.Add(c, item); err == memcache.ErrNotStored {
+                c.Warningf("item with key %q already exists", item.Key)
+            } else if err != nil {
+                c.Warningf("error adding item: %v", err)
+            }
+            break
+        }
+    }
 
-	if thumb.Pic == nil {
-		fmt.Fprint(w, "<html><head> ", style, "<title>;(</title></head><body>")
-		fmt.Fprint(w, "<h4>can't find thumb for ", name)
-		return
-	}
+    if thumb.Pic == nil {
+        fmt.Fprint(w, "<html><head> ", style, "<title>;(</title></head><body>")
+        fmt.Fprint(w, "<h4>can't find thumb for ", name)
+        return
+    }
 
     w.Header().Set("Content-Type", "image/jpeg")
     w.Header().Set("Content-Length", fmt.Sprintf("%v",len(thumb.Pic)) )
 
-	io.Copy( w, bytes.NewBuffer(thumb.Pic) )
+    io.Copy( w, bytes.NewBuffer(thumb.Pic) )
 }
 
 
 func handler_reset(w http.ResponseWriter, r *http.Request) {
-	persons = nil
-	http.Redirect(w, r, "/", http.StatusFound)
+    persons = nil
+    http.Redirect(w, r, "/", http.StatusFound)
 }
 
 
 // sortable tmp array of recognition results
 type KV struct {
-	p string
+    p string
     d float64
 }
 type KVarr []KV 
-func (s KVarr) Len() int 		   { return len(s) }
+func (s KVarr) Len() int            { return len(s) }
 func (s KVarr) Less(i, j int) bool { return s[i].d < s[j].d }
 func (s KVarr) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 func compare(match string, hist *Histogram) (compres string) {
-	if hist==nil { return }
-	if len(persons)<2 { return }
-	compres = fmt.Sprintf("&nbsp;&nbsp;<img src='/thumb?id=%v'> &nbsp;&nbsp; matches for : <i>%v</i> <br><br>\n",match,match)
-	kv := make(KVarr,len(persons)-1)
-	i := 0
-	mm := 0.0
-	for n,h := range persons {
-		if match == n { continue }
-		dist := norml2(h,hist)
-		kv[i] = KV{p:n,d:dist}
-		mm = math.Max(mm,dist)
-		i += 1
-	}
-	sort.Sort(kv)
-	for i=0; i<int(math.Min(10.0,float64(len(kv)))); i++ {
-		confidence := 1.0-(kv[i].d/mm)
-		zz:=""; 
-		if confidence > 0.59 { zz = "style='border-style:solid;'"}
-		compres += fmt.Sprintf("&nbsp;&nbsp;<a href='/?match=%v' title='%v : %3.3f'><img src='/thumb?id=%v' %v></a>\n", kv[i].p, kv[i].p, confidence, kv[i].p, zz) 
-	}
-	return 
+    if hist==nil { return }
+    if len(persons)<2 { return }
+    compres = fmt.Sprintf("&nbsp;&nbsp;<img src='/thumb?id=%v'> &nbsp;&nbsp; matches for : <i>%v</i> <br><br>\n",match,match)
+    kv := make(KVarr,len(persons)-1)
+    i := 0
+    mm := 0.0
+    for n,h := range persons {
+        if match == n { continue }
+        dist := norml2(h,hist)
+        kv[i] = KV{p:n,d:dist}
+        mm = math.Max(mm,dist)
+        i += 1
+    }
+    sort.Sort(kv)
+    for i=0; i<int(math.Min(10.0,float64(len(kv)))); i++ {
+        confidence := 1.0-(kv[i].d/mm)
+        zz:=""; 
+        if confidence > 0.59 { zz = "style='border-style:solid;'"}
+        compres += fmt.Sprintf("&nbsp;&nbsp;<a href='/?match=%v' title='%v : %3.3f'><img src='/thumb?id=%v' %v></a>\n", kv[i].p, kv[i].p, confidence, kv[i].p, zz) 
+    }
+    return 
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
     r.ParseMultipartForm(1000000)
-	if persons == nil { fillDict(c) }
-	hr := ""
-	var m *Matrix = nil
-	var hist *Histogram = nil
-	
-	name := r.FormValue("n");
-	if _,e := persons[name]; e {
-		hist = persons[name]
-	} else {
-		f, fhead, err := r.FormFile("f")
-		if err == nil {
-			defer f.Close()
-			if name=="" { name = fmt.Sprintf("%v",fhead)[2:12] }
-			// Grab the image data
-			var buf bytes.Buffer
-			io.Copy(&buf, f)
-			im, imtype, err := image.Decode(&buf)
-			if err == nil {
-				b := im.Bounds()
-				//~ if v:=r.FormValue("x"); v!="" { if x,e := strconv.Atoi(v); e==nil { b.Min.X=x } }
-				//~ if v:=r.FormValue("y"); v!="" { if x,e := strconv.Atoi(v); e==nil { b.Min.Y=x } }
-				//~ if v:=r.FormValue("w"); v!="" { if x,e := strconv.Atoi(v); e==nil { b.Max.X=b.Min.X + x } }
-				//~ if v:=r.FormValue("h"); v!="" { if x,e := strconv.Atoi(v); e==nil { b.Max.Y=b.Min.Y + x } }
-				hr = fmt.Sprintf("%v %v %v", name,imtype, b)
-				c.Infof(hr)
-				im = Resize(im, b, 90, 90)
-				switch it := im.(type) {
-				case *image.RGBA:
-					m = handleRGBA(im.(*image.RGBA))
-					break
-				case *image.YCbCr:
-					m = handleYCbCr(im.(*image.YCbCr))
-					break
-				default:
-					c.Warningf("can't resolve image type %v %v", it,imtype)
-				}
-				if m != nil {
-					hist = m.histogram(circle, 1)
-					persons[name] = hist
-				}
-				// it's a new one, so save a thumbnail
-				buf.Reset()
-				jpeg.Encode(&buf, Resize(im, im.Bounds(), 50, 50), nil)
-				thumb := Thumbnail{ Name:name, Pic:buf.Bytes()}
-				datastore.Put(c, datastore.NewIncompleteKey(c, "Thumbnail", nil), &thumb)  						
-			} else {
-				c.Warningf("%v",err)
-			}
-		} else {
-			c.Warningf("%v",err)
-		}
-	}
+    if persons == nil { fillDict(c) }
+    hr := ""
+    var m *Matrix = nil
+    var hist *Histogram = nil
+    
+    name := r.FormValue("n");
+    if _,e := persons[name]; e {
+        hist = persons[name]
+    } else {
+        f, fhead, err := r.FormFile("f")
+        if err == nil {
+            defer f.Close()
+            if name=="" { name = fmt.Sprintf("%v",fhead)[2:12] }
+            // Grab the image data
+            var buf bytes.Buffer
+            io.Copy(&buf, f)
+            im, imtype, err := image.Decode(&buf)
+            if err == nil {
+                b := im.Bounds()
+                //~ if v:=r.FormValue("x"); v!="" { if x,e := strconv.Atoi(v); e==nil { b.Min.X=x } }
+                //~ if v:=r.FormValue("y"); v!="" { if x,e := strconv.Atoi(v); e==nil { b.Min.Y=x } }
+                //~ if v:=r.FormValue("w"); v!="" { if x,e := strconv.Atoi(v); e==nil { b.Max.X=b.Min.X + x } }
+                //~ if v:=r.FormValue("h"); v!="" { if x,e := strconv.Atoi(v); e==nil { b.Max.Y=b.Min.Y + x } }
+                hr = fmt.Sprintf("%v %v %v", name,imtype, b)
+                c.Infof(hr)
+                im = Resize(im, b, 90, 90)
+                switch it := im.(type) {
+                case *image.RGBA:
+                    m = handleRGBA(im.(*image.RGBA))
+                    break
+                case *image.YCbCr:
+                    m = handleYCbCr(im.(*image.YCbCr))
+                    break
+                default:
+                    c.Warningf("can't resolve image type %v %v", it,imtype)
+                }
+                if m != nil {
+                    hist = m.histogram(circle, 1)
+                    persons[name] = hist
+                }
+                // it's a new one, so save a thumbnail
+                buf.Reset()
+                jpeg.Encode(&buf, Resize(im, im.Bounds(), 50, 50), nil)
+                thumb := Thumbnail{ Name:name, Pic:buf.Bytes()}
+                datastore.Put(c, datastore.NewIncompleteKey(c, "Thumbnail", nil), &thumb)                          
+            } else {
+                c.Warningf("%v",err)
+            }
+        } else {
+            c.Warningf("%v",err)
+        }
+    }
 
 
-	compres := ""
-	if hist != nil {
-		compres = compare(name, hist)
-		
-		buf := new(bytes.Buffer)
-		rr  := binary.Write(buf, binary.LittleEndian, hist)
-		if rr != nil {
-			c.Warningf("%v",rr)
-		}
-		nb := buf.Bytes()
+    compres := ""
+    if hist != nil {
+        compres = compare(name, hist)
+        
+        buf := new(bytes.Buffer)
+        rr  := binary.Write(buf, binary.LittleEndian, hist)
+        if rr != nil {
+            c.Warningf("%v",rr)
+        }
+        nb := buf.Bytes()
 
-			//~ var b bytes.Buffer
-			//~ w := zlib.NewWriter(&b)
-			//~ w.Write(buf.Bytes())
-			//~ w.Close()
-			//~ nb = b.Bytes()
-			
-		p := PicRec { Name: name, Hist: nb }
-		_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "PicRec", nil), &p)    
-		if err != nil {
-			c.Warningf("%v",err)
-		}
-		
-	} else if match := r.FormValue("match"); match!="" { 
-		compres = compare(match,persons[match])
-	}
-	
-	
+            //~ var b bytes.Buffer
+            //~ w := zlib.NewWriter(&b)
+            //~ w.Write(buf.Bytes())
+            //~ w.Close()
+            //~ nb = b.Bytes()
+            
+        p := PicRec { Name: name, Hist: nb }
+        _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "PicRec", nil), &p)    
+        if err != nil {
+            c.Warningf("%v",err)
+        }
+        
+    } else if match := r.FormValue("match"); match!="" { 
+        compres = compare(match,persons[match])
+    }
+    
+    
     w.Header().Add("Content-Type", "text/html")
     fmt.Fprint(w, "<html><head> ", style, "<title></title></head><body>\n")
     //~ fmt.Fprint(w, "<form method=POST enctype='multipart/form-data' onSubmit='postCanvasToURL()'>\n")
@@ -320,9 +320,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
     //~ fmt.Fprint(w, "<input type=hidden name='h'><p>\n")
     fmt.Fprint(w, "<p><hr NOSHADE>\n")
     fmt.Fprint(w, compres, "<p><hr NOSHADE>", listDict(),"<p><hr NOSHADE>" )
-	fmt.Fprint(w, "<script type='text/javascript' src='js/ccv.js'></script>\n")
-	fmt.Fprint(w, "<script type='text/javascript' src='js/face.js'></script>\n")
-	fmt.Fprint(w, "<script type='text/javascript' src='js/faceup.js'></script>")
+    fmt.Fprint(w, "<script type='text/javascript' src='js/ccv.js'></script>\n")
+    fmt.Fprint(w, "<script type='text/javascript' src='js/face.js'></script>\n")
+    fmt.Fprint(w, "<script type='text/javascript' src='js/faceup.js'></script>")
     fmt.Fprintf(w, "%v\n", hr)
     fmt.Fprint(w, "</body></html>")
 }
